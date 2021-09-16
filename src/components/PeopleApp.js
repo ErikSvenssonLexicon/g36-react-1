2af2b345617a147dbf1c2cc4fa704c5573e5c71f
Erik Svensson <eosvensson@gmail.com>
2021-09-16 09:59:47 +0200

    Now able to add people

--- src/components/PeopleApp.js
+++ src/components/PeopleApp.js
@@ -1,29 +1,39 @@
+import {useState} from 'react';
 import PersonForm from "./forms/PersonForm";
 import Person from "./Person";
 
-const PeopleApp = (props) => {
-  const person = {
-    id: "foo1",
-    firstName: "Erik",
-    lastName: "Svensson",
-    birthDate: "1976-09-11",
-    getAge() {
-      const today = new Date();
-      const date = new Date(this.birthDate);
+const person = {
+  id: 34252,
+  firstName: "Erik",
+  lastName: "Svensson",
+  birthDate: "1976-09-11",
+  getAge() {
+    const today = new Date();
+    const date = new Date(this.birthDate);
 
-      let age = today.getFullYear() - date.getFullYear();
-      const months = today.getMonth() - date.getMonth();
-      if (months < 0 || (months === 0 && today.getDate() < date.getDate())) {
-        age--;
-      }
-      return age;
-    },
-  };
+    let age = today.getFullYear() - date.getFullYear();
+    const months = today.getMonth() - date.getMonth();
+    if (months < 0 || (months === 0 && today.getDate() < date.getDate())) {
+      age--;
+    }
+    return age;
+  },
+};
+
+const PeopleApp = (props) => {
+
+  const [people, setPeople] = useState([person])
+
+
+  const handleAddPerson = (person) =>{    
+    setPeople([...people, person])  
+  }
+
+  
 
   return (
     <div className="container">
-      <PersonForm />
-      <Person person={person} />
+      <PersonForm handleAddPerson={handleAddPerson} />      
     </div>
   );
 };
--- src/components/forms/PersonForm.js
+++ src/components/forms/PersonForm.js
@@ -1,15 +1,49 @@
-const PersonForm = () => {
+import { useState } from "react";
+import { createPerson } from "../object-factories/factoryFunctions";
+
+const PersonForm = (props) => {
+  const [firstNameValue, setFirstNameValue] = useState("");
+  const [lastNameValue, setLastNameValue] = useState("");
+  const [birthDateValue, setBirthDateValue] = useState("");
+
   const min = "1910-01-01";
   const max = "2021-09-15";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    //Create person
+    const person = createPerson(firstNameValue, lastNameValue, birthDateValue);
+    //Call handle method passed down through props
+    props.handleAddPerson(person);
+    //Clear the input values
+    setFirstNameValue("");
+    setLastNameValue("");
+    setBirthDateValue("");
+  }
+
+  function handleFirstNameChange(event) {
+    setFirstNameValue(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastNameValue(event.target.value);
+  }
+
+  function handleBirthDateChange(event) {
+    setBirthDateValue(event.target.value);
+  }
 
   return (
     <div className="card">
       <div className="card-body">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row g-2 mb-2">
             <div className="col">
               <div className="form-floating">
                 <input
+                  onChange={handleFirstNameChange}
+                  value={firstNameValue}
                   type="text"
                   className="form-control"
                   id="firstName"
@@ -21,6 +55,8 @@ const PersonForm = () => {
             <div className="col">
               <div className="form-floating">
                 <input
+                  onChange={handleLastNameChange}
+                  value={lastNameValue}
                   type="text"
                   className="form-control"
                   id="lastName"
@@ -32,6 +68,8 @@ const PersonForm = () => {
           </div>
           <div className="form-floating mb-2">
             <input
+              onChange={handleBirthDateChange}
+              value={birthDateValue}
               type="date"
               className="form-control"
               id="birthDate"
